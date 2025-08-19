2d01392713b1b1003556535a44ce80c4c8579f8a
CaIagFormal <carlos.tecnico.iagnecz@gmail.com>
2025-08-19 17:27:05 -0300

    Esqueci de alterar CustomUserDetailsService.java

--- volante-backend/src/main/java/com/exp_backend/volante/autenticacao/spring_security/service/CustomUserDetailsService.java
+++ volante-backend/src/main/java/com/exp_backend/volante/autenticacao/spring_security/service/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.exp_backend.volante.autenticacao.spring_security.service;
 
+import com.exp_backend.volante.autenticacao.spring_security.model.M_Usuario;
 import com.exp_backend.volante.autenticacao.spring_security.model.UserPrincipal;
+import com.exp_backend.volante.autenticacao.spring_security.repository.R_Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,21 +16,20 @@ import org.springframework.stereotype.Service;
 public class CustomUserDetailsService implements UserDetailsService {
 
     // AQUI É O REPOSITORY DO MODEL DO USUÁRIO
-    /*@Autowired
-    private R_Usuario r_usuario;*/
+    @Autowired
+    private R_Usuario r_usuario;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // AQUI É AUTO EXPLICATIVO. REMOVER RETURN NULL DEPOIS QUE AS CLASSES FOREM IMPLEMENTADAS
 
-        /*M_Usuario usuario = r_usuario.findByUsername(username);
+        M_Usuario usuario = r_usuario.findByUsername(username);
 
         if(usuario == null){
             System.out.println("User not found");
             throw new UsernameNotFoundException("User not found");
         }
 
-        return new UserPrincipal(usuario);*/
-        return null;
+        return new UserPrincipal(usuario);
     }
 }
